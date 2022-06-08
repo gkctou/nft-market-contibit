cf14bbce3cbac6079f0a024c696e8b1b17ea9b42
Jason Chen <gkctou@gmail.com>
2022-06-09 02:26:22 +0800

    save

--- packages/frontend/src/combinations/LoginCard.vue
+++ packages/frontend/src/combinations/LoginCard.vue
@@ -1,53 +1,58 @@
 <template>
-    <q-card square class="shadow-16" style="width:400px;height:500px">
-        <q-card-section class="bg-primary text-white">
-            <h5 class="text-h5 q-my-md">Login</h5>
-            <!-- <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
+  <q-card square class="shadow-16" style="width:400px;height:520px">
+    <q-card-section class="bg-primary text-white">
+      <h5 class="text-h5 q-my-md">Login</h5>
+      <!-- <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
                 <q-btn fab icon="add" color="purple-4" />
             </div> -->
-        </q-card-section>
-        <q-card-section>
-            <q-form class="q-px-sm q-py-sm">
-                <q-input square clearable v-model="email" type="email" label="Email">
-                    <template v-slot:prepend>
-                        <q-icon name="email" />
-                    </template>
-                </q-input>
+    </q-card-section>
+    <q-card-section>
+      <q-form class="q-px-sm q-py-sm">
+        <q-input square clearable v-model="email" type="email" label="Email">
+          <template v-slot:prepend>
+            <q-icon name="email" />
+          </template>
+        </q-input>
 
-                <!-- <InputCountDown :dense="true" /> -->
-
-                <q-input square clearable v-model="password" type="password" label="Password">
-                    <template v-slot:prepend>
-                        <q-icon name="lock" />
-                    </template>
-                </q-input>
-            </q-form>
-        </q-card-section>
-        <q-card-section>
-            <div class="text-center q-pa-md q-gutter-md">
-                <q-btn round color="indigo-7">
-                    <q-icon name="fab fa-facebook-f" size="1.2rem" />
-                </q-btn>
-                <q-btn round color="red-8">
-                    <q-icon name="fab fa-google-plus-g" size="1.2rem" />
-                </q-btn>
-                <q-btn round color="light-blue-5">
-                    <q-icon name="fab fa-twitter" size="1.2rem" />
-                </q-btn>
-            </div>
-        </q-card-section>
-        <q-card-actions class="q-px-lg">
-            <q-btn unelevated size="lg" color="primary" class="full-width text-white" label="Sign In" />
-        </q-card-actions>
-        <q-card-section class="text-center q-pa-sm">
-            <p class="text-grey-6">Forgot your password?{{$q.screen.gt.xs?'gt':'xs'}}</p>
-        </q-card-section>
-    </q-card>
+        <q-input square clearable v-model="password" type="password" label="Password">
+          <template v-slot:prepend>
+            <q-icon name="lock" />
+          </template>
+          <template v-slot:append>
+            <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+          </template>
+        </q-input>
+      </q-form>
+    </q-card-section>
+    <!-- <q-card-section>
+      <div class="text-center q-pa-md q-gutter-md">
+        <q-btn round color="indigo-7">
+          <q-icon name="fab fa-facebook-f" size="1.2rem" />
+        </q-btn>
+        <q-btn round color="red-8">
+          <q-icon name="fab fa-google-plus-g" size="1.2rem" />
+        </q-btn>
+        <q-btn round color="light-blue-5">
+          <q-icon name="fab fa-twitter" size="1.2rem" />
+        </q-btn>
+      </div>
+    </q-card-section> -->
+    <q-card-actions class="q-px-lg">
+      <divs style="height: 130px;"></divs>
+    </q-card-actions>
+    <q-card-actions class="q-px-lg">
+      <q-btn unelevated size="lg" color="primary" class="full-width text-white" label="Sign In" />
+    </q-card-actions>
+    <q-card-section class="text-center q-pa-sm">
+      <p class="text-grey-6">Forgot your password?{{ $q.screen.gt.xs ? 'gt' : 'xs' }}</p>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script setup lang="ts">
-import InputCountDown from '../components/InputCountDown.vue';
-
+// import InputCountDown from '../components/InputCountDown.vue';
+import { $ref } from 'vue/macros';
+let isPwd = $ref(true);
 const props = withDefaults(defineProps<{
   email?: string
   password?: string
@@ -58,7 +63,7 @@ const props = withDefaults(defineProps<{
 });
 
 const emit = defineEmits<{
-    (e: 'success', token: string): void
-    (e: 'fail', message: string): void
+  (e: 'success', token: string): void
+  (e: 'fail', message: string): void
 }>();
 </script>
--- packages/frontend/src/combinations/RegisterCard.vue
+++ packages/frontend/src/combinations/RegisterCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-card square class="shadow-16" style="width:400px;height:auto;">
+  <q-card square class="shadow-16" style="width:400px;height:520px;">
     <q-card-section class="bg-primary text-white">
       <h5 class="text-h5 q-my-md">Registration</h5>
       <!-- <div class="absolute-bottom-right q-pr-md" style="transform: translateY(50%);">
@@ -13,38 +13,34 @@
             <q-icon name="email" />
           </template>
         </q-input>
-        <InputCountDown icon-name="mail_lock" :sendable="true" :next-on="Date.now() + 60000" dense />
-        <q-input square clearable v-model="email" type="email" label="Email" dense>
+        <InputCountDown icon-name="mail_lock" :sendable="true" :next-on="Date.now() + 60000" dense hint="Receive verify code from your email box." label="Email Verify" />
+        <q-input square clearable v-model="email" type="email" label="Mobile Phone" dense>
           <template v-slot:prepend>
-            <q-icon name="email" />
+            <q-icon name="phone_iphone" />
           </template>
         </q-input>
-        <InputCountDown :sendable="true" :next-on="Date.now() + 60000" dense/>
-        <q-input bottom-slots v-model="text" label="Label" counter dense>
-          <template v-slot:prepend>
-            <q-icon name="place" />
-          </template>
-          <template v-slot:append>
-            <q-icon name="close" @click="text = ''" class="cursor-pointer" />
-          </template>
-
-          <template v-slot:hint>
-            Field hint
-          </template>
-        </q-input>
-        <q-input square clearable v-model="password" type="password" label="Password" dense>
+        <InputCountDown :sendable="true" :next-on="Date.now() + 60000" dense />
+        <q-input square clearable  bottom-slots v-model="text" type="password" label="Password" hint="Minimum length of 8 characters" dense>
           <template v-slot:prepend>
             <q-icon name="lock" />
           </template>
         </q-input>
+        <q-input square clearable v-model="password" type="password" label="Retype Password" dense>
+          <template v-slot:prepend>
+            <q-icon name="lock" />
+          </template>
+          <template v-slot:append>
+            <q-icon :name="isPwd ? 'visibility_off' : 'visibility'" class="cursor-pointer" @click="isPwd = !isPwd" />
+          </template>
+        </q-input>
       </q-form>
     </q-card-section>
-    <q-card-actions class="q-px-lg">
+    <q-card-actions class="q-px-lg q-pb-lg">
       <q-btn unelevated size="lg" color="primary" class="full-width text-white" label="Get Started" />
     </q-card-actions>
-    <q-card-section class="text-center q-pa-sm">
-      <!-- <p class="text-grey-6">Return to login</p> -->
-    </q-card-section>
+    <!-- <q-card-section class="text-center q-pa-sm">
+      <p class="text-grey-6">&nbsp;</p>
+    </q-card-section> -->
   </q-card>
 </template>
 
@@ -52,6 +48,7 @@
 import InputCountDown from '../components/InputCountDown.vue';
 // import { ref } from 'vue';
 import { $ref } from 'vue/macros';
+let isPwd = $ref(true);
 let dense = $ref(false);
 let text = $ref('');
 let email = $ref('');
