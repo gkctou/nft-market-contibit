d2b06b7b96c64996f09932ba62b7642e7ab2054e
Jason Chen <gkctou@gmail.com>
2022-06-10 09:38:59 +0800

    save

--- packages/frontend/src/pages/IndexPage.vue
+++ packages/frontend/src/pages/IndexPage.vue
@@ -2,8 +2,14 @@
   <h1 class="text-3xl font-bold underline">
     Hello world!
   </h1>
+  <hr/>
+  <h1 class="text-h1">HELLO, World.</h1>
+  <h2 class="text-h2">HELLO, World.</h2>
+  <h3>HELLO, World.</h3>
+  <h4>HELLO, World.</h4>
+  <h5>HELLO, World.</h5>
   <MountedTeleport to="#LeftDrawer">
-    <h5>Mount from home</h5>
+    <h1>Mount from home</h1>
   </MountedTeleport>
 </template>
 
