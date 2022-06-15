8e5763511c6aa03eb9f31a60eb1d6d735a085ae3
Jason Chen <gkctou@gmail.com>
2022-06-15 09:15:11 +0800

    save

--- packages/datacenter/src/index.ts
+++ packages/datacenter/src/index.ts
@@ -1,7 +1,9 @@
 import path from 'path';
 import fs from 'fs';
 import ObjHash from 'node-object-hash';
-const hasher = ObjHash({coerce: {set: true, symbol: true}});
+import LRU from 'lru-cache';
+
+const hasher = ObjHash({ coerce: { set: true, symbol: true } });
 const cachPath = path.resolve(__dirname, '../tmp/cache/');
 const storagePath = path.resolve(__dirname, '../tmp/storage/');
 if (!fs.existsSync(path.resolve(__dirname, '../tmp/')))
@@ -10,3 +12,32 @@ if (!fs.existsSync(cachPath))
     fs.mkdirSync(cachPath);
 if (!fs.existsSync(storagePath))
     fs.mkdirSync(storagePath);
+const lruOptions = {
+    max: 5000,
+    // for use with tracking overall storage size
+    maxSize: 5000,
+    sizeCalculation: (value, key) => {
+        return 1
+    },
+    // for use when you need to clean up something when objects
+    // are evicted from the cache
+    // dispose: (value, key) => {
+    //     freeFromMemoryOrWhatever(value)
+    // },
+    // how long to live in ms
+    ttl: 1000 * 60 * 60,
+    // return stale items before removing from cache?
+    allowStale: false,
+    updateAgeOnGet: false,
+    updateAgeOnHas: false,
+    // async method to use for cache.fetch(), for
+    // stale-while-revalidate type of behavior
+    // fetch: async (key, staleValue, { options, signal }) => { }
+}
+const Lru = new LRU(lruOptions);
+export async function cacheSet<T = any>(key: string, value: T): Promise<void> {
+
+}
+export async function cacheGet<T = any>(key: string): Promise<T> {
+
+}
